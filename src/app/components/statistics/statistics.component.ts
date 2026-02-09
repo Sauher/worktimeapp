842a55aefc3e96f2f3a31f052610c312848133a3
Sauher <schaueroliver@turr.hu>
2026-02-09 12:29:24 +0100

    pushpush

--- src/app/components/statistics/statistics.component.ts
+++ src/app/components/statistics/statistics.component.ts
@@ -4,7 +4,8 @@ import { ChartModule } from 'primeng/chart';
 import { DatePicker } from 'primeng/datepicker';
 import { ApiService } from '../../services/api.service';
 import { User } from '../../interfaces/user';
-import { RouteReuseStrategy } from '@angular/router';
+import moment from 'moment';
+import { WorkTime } from '../../interfaces/worktime';
 
 @Component({
   selector: 'app-statistics',
@@ -17,6 +18,11 @@ export class StatisticsComponent implements OnInit {
   date: Date = new Date();
   data: any;
   users: User[] = [];
+  monthDays: string[] = [];
+  sumMinutesPerDay : number[] = []
+  countUserWithDataPerDay : number[] = []
+  avgWorkTimePerDays : number[] = []
+
 
     options: any;
 
@@ -26,6 +32,7 @@ export class StatisticsComponent implements OnInit {
       this.getUsers();
       this.refreshChart();
       this.renderChart();
+      
     }
     renderChart(){
        const documentStyle = getComputedStyle(document.documentElement);
@@ -92,14 +99,45 @@ export class StatisticsComponent implements OnInit {
             }
         };
     }
-    getMonthDays(){}
+    getMonthDays(){
+      const y = this.date.getFullYear()
+      const m = this.date.getMonth();
+      this.monthDays = []
+      const first = new Date(y,m,1)
+      const last = new Date(y,m+1,0)
+
+      for(let d = first; d<= last; d.setDate(d.getDate()+1)){
+        this.monthDays.push(moment(d).format('MM-DD'))
+      }
+      
+    }
 
     getAvgWorkTimes(){}
 
-    getUserWorkTimes(userId:string){}
+    getUserWorkTimes(userId:string){
+      this.api.selectByField('worktimes','userId','eq',userId).subscribe({
+        next: (res)=>{
+          const worktimes = res as WorkTime[]
+
+
+          const filteredWorktimes = worktimes.filter(w=> moment(w.date).format('YYYY-MM') == moment(this.data).format('YYYY-MM'))
+          
+          filteredWorktimes.forEach(fw => {
+            
+          })
+        }
+      })
+    }
     
     refreshChart(){
+
+
       this.getMonthDays();
+
+      this.sumMinutesPerDay = new Array(this.monthDays.length).fill(0)
+      this.countUserWithDataPerDay = new Array(this.monthDays.length).fill(0)
+      this.avgWorkTimePerDays  = new Array(this.monthDays.length).fill(0)
+
       this.getAvgWorkTimes();
       this.users.forEach(item => {
         this.getUserWorkTimes(item.id);
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  serverUrl: 'http://localhost:4000',
+  serverUrl: 'http://localhost:3000',
   tokenName: 'worktimeapp'
 };
